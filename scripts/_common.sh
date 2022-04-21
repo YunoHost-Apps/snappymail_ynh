729bbcdfc8dc6a9fa66a04590583a2c85e0f9a91
ericgaspar <junk.eg@free.fr>
2022-04-21 18:30:00 +0200

    First commit

---
+++ scripts/_common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="7.4"
+
+pkg_dependencies="php-uuid php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
