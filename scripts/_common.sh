bbeda841d3b4c09f41d4e643bec0fe109c965782
ericgaspar <junk.eg@free.fr>
2022-04-27 17:04:08 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="php-uuid php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring"
+pkg_dependencies="php-uuid php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
 # PERSONAL HELPERS
