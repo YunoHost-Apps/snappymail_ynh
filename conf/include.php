7dcd8289f23379e62313c9787f4eab62a664fa5e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-10-13 09:29:20 +0200

    Update include.php

---
+++ conf/include.php
@@ -0,0 +1,30 @@
+<?php
+
+// Name this file as "include.php" to enable it.
+
+//header('Strict-Transport-Security: max-age=31536000');
+
+/**
+ * Uncomment to enable multiple domain installation.
+ */
+//define('MULTIDOMAIN', 1);
+
+/**
+ * Uncomment to disable APCU.
+ */
+//define('APP_USE_APCU_CACHE', false);
+
+/**
+ * Custom 'data' folder path
+ */
+define('APP_DATA_FOLDER_PATH', '__DATA_DIR__');
+
+/**
+ * Additional configuration file name
+ */
+//define('APP_CONFIGURATION_NAME', $_SERVER['HTTP_HOST'].'.ini');
+
+/**
+ * Also update extensions on upgrade
+ */
+//define('SNAPPYMAIL_UPDATE_PLUGINS', 1);
