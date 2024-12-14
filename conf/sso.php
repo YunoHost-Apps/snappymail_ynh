fef1b90a275ca88c92e9b4b98cfe2080d29e905d
Tagada <36127788+Tagadda@users.noreply.github.com>
2024-12-14 14:34:07 +0100

    Fix SSO

--- conf/sso.php
+++ conf/sso.php
@@ -5,8 +5,8 @@ $_ENV['SNAPPYMAIL_INCLUDE_AS_API'] = true;
 require 'app/index.php';
 
 // Retrieve email and password
-if (isset($_SERVER['HTTP_EMAIL']) && isset($_SERVER['PHP_AUTH_PW'])) {
-        $email = $_SERVER['HTTP_EMAIL'];
+if (isset($_SERVER['HTTP_YNH_USER_EMAIL']) && isset($_SERVER['PHP_AUTH_PW'])) {
+        $email = $_SERVER['HTTP_YNH_USER_EMAIL'];
         $password = $_SERVER['PHP_AUTH_PW'];
         $ssoHash = \RainLoop\Api::CreateUserSsoHash($email, $password);
         // redirect to webmail sso url
